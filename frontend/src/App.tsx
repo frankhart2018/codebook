3da169ef948fe4224b13ddef3d382afaa1db143e
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-08-30 15:52:13 -0400

    Add endpoint to parse markdown

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,15 +9,7 @@ import "./App.css";
 const App = (): JSX.Element => {
   document.title = "CodeBook";
 
-  const input = `
-  # Watching for changes on a website
-
-  Recently I had to apply for a visa, and to get an appointment you need to go to the government website and constantly check if there are new spots available. And you need to be quick, as the free spots don't last long.
-  
-  So I decided to write a bot, that navigates through the forms every 10 minutes and send me a message on Telegram as soon the state of it changes. The bot has two parts, the first is the code to interact with selenium webdriver, and the second is to alert about changes through Telegram.
-  
-  ~~>>>test-1.py<<<~~
-  `;
+  const input = ;
 
   return (
     <Grid container spacing={0}>
